068fdc00cec99ea8fdffd66c31c12d831384abd7
Dezzz <dezzz@Dezzzs-MacBook-Air.local>
2023-02-10 18:04:34 -0500

    Lab 2a

---
+++ E1.cpp
@@ -0,0 +1,8 @@
+//
+//  E1.cpp
+//  
+//
+//  Created by Dezzz on 2/3/23.
+//
+
+#include <stdio.h>
---
+++ valid.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Deziah Gayle
+Course: CSCI-135
+Instructor: Mike Z.; Genedy M.
+Assignment: title, e.g., Lab1A
+
+Input Valdiation
+*/
+
+#include <iostream>
+
+int main() {
+  int num;
+  do {
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+  } while (num <= 0 || num >= 100);
+
+  int square = num * num;
+  std::cout << "The square of " << num << " is " << square << std::endl;
+  return 0;
+}
